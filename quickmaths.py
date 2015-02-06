d015113dea2d7cf415bddd98a1b130881245d40b
Matthew Darby <me@mjdarby.net>
2015-02-06 00:35:10 +0000

    Added the 'game' proper

--- quickmaths.py
+++ quickmaths.py
@@ -25,7 +25,8 @@
 
 import sys
 from random import randint, choice
-
+from math import sqrt
+from time import time
 ## Formula stuff.
 
 class Formula:
@@ -100,7 +101,7 @@ class DivisionFormula(Formula):
         self.right = right
 
     def value(self):
-        return self.left.value() / self.right.value()
+        return self.left.value() // self.right.value()
 
     def text(self):
         return "({} / {})".format(self.left.text(), self.right.text())
@@ -126,7 +127,16 @@ class QuestionParameter:
     def generateFormula(self):
         left = NumberFormula(randint(self.bottomValue, self.topValue))
         right = NumberFormula(randint(self.bottomValue, self.topValue))
-        aFormula = self.formula(left ,right)
+
+        # Division is special, as we target a specific value instead
+        if self.formula == DivisionFormula:
+            target = randint(self.bottomValue, self.topValue)
+            firstFactor = randint(2, int(sqrt(self.topValue)))
+            secondFactor = randint(2, 8)
+            left = NumberFormula(target * firstFactor * secondFactor)
+            right = NumberFormula(firstFactor * secondFactor)
+
+        aFormula = self.formula(left, right)
         return aFormula
 
 class QuestionGenerator:
@@ -134,14 +144,77 @@ class QuestionGenerator:
         self.parameters = parameters
 
     def generate(self):
-        """Pick a random parameter and generate a question from it"""
+        """Pick a random parameter and generate a question"""
         aParameter = choice(self.parameters)
         aFormula = aParameter.generateFormula()
         aQuestion = Question(aFormula)
         return aQuestion
 
-if __name__ == "__main__":
-    aParameter = QuestionParameter(SumFormula, 5, 10)
+operationToFormula = {"sum": SumFormula,
+                      "dif": DifferenceFormula,
+                      "pro": ProductFormula,
+                      "div": DivisionFormula}
+
+def printUsage():
+    progName = sys.argv[0]
+    print("""Usage: {} - Print this usage information
+       {} [operation] [lowRange] [highRange]""".format(progName, progName))
+
+def checkArguments():
+    aBottomValue = int(sys.argv[2])
+    aTopValue = int(sys.argv[3])
+    if aBottomValue >= aTopValue:
+        print("The lower value of the range must be smaller than the higher value!")
+        return False
+    if sys.argv[1] not in operationToFormula.keys():
+        print("Pick one of 'pro', 'dif', 'sum' or 'div'")
+        return False
+    return True
+
+def beginGame():
+    aFormulaClass = operationToFormula[sys.argv[1]]
+    aBottomValue = int(sys.argv[2])
+    aTopValue = int(sys.argv[3])
+    aParameter = QuestionParameter(aFormulaClass, aBottomValue, aTopValue)
     aGenerator = QuestionGenerator([aParameter])
     myQuestion = aGenerator.generate()
-    print(myQuestion.formula.text() + " = " + str(myQuestion.formula.value()))
+    userString = ""
+    totalAttempts = 0
+    totalCorrect = 0
+    averageTime = 0
+    timeTaken = time()
+    # This run loop is not my finest hour.
+    while userString != "exit":
+        print("{} > ".format(myQuestion.formula.text()), end='')
+
+        userString = input()
+        try:
+            userAnswer = int(userString)
+            if userAnswer == myQuestion.formula.value():
+                print("Correct, next!")
+                myQuestion = aGenerator.generate()
+                timeTaken = time() - timeTaken
+                averageTime = (averageTime * totalCorrect) + timeTaken
+                totalCorrect += 1
+                averageTime /= totalCorrect
+                timeTaken = time()
+            else:
+                print("Incorrect, try again!")
+            totalAttempts += 1
+
+        except Exception as e:
+            pass
+    if totalAttempts == 0:
+        pass
+    else:
+        print("Out of {} attempted answers, you got {}% right! On average, you took {} seconds to answer a question correctly.".format(totalAttempts,
+                                                                                                                                       totalCorrect*100/totalAttempts,
+                                                                                                                                       averageTime))
+
+
+if __name__ == "__main__":
+    if (len(sys.argv) == 1):
+        printUsage()
+    elif (len(sys.argv) == 4):
+        if checkArguments():
+            beginGame()
